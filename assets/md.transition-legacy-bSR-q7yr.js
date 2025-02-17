9a092bd1d45dc467b535fd0768627bd05851d8c6
cathcath16 <20221509@nbsc.edu.ph>
2025-02-17 18:30:56 +0800

    Updates

--- assets/md.transition-legacy-bSR-q7yr.js
+++ assets/md.transition-legacy-bSR-q7yr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-k__nn6BQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-qrHP93r1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
