57b12f0d0ade30b9eb072cbc100a5da1a6f87f4e
cathcath16 <catherenelabinisia@gmail.com>
2025-03-06 20:56:35 +0800

    added a login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,64 @@
 import { 
-    IonButton,
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
+  IonButton,
+  IonContent, 
+  IonHeader, 
+  IonInput, 
+  IonItem, 
+  IonLabel, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter 
+} from '@ionic/react';
+import { useState } from 'react';
+
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
   
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
-    return (
+  // State to hold username and password
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const doLogin = () => {
+      // Here you can add your login logic (e.g., API call)
+      console.log('Username:', username);
+      console.log('Password:', password);
+      
+      // Navigate to the next page after login
+      navigation.push('/it35-lab/app', 'forward', 'replace');
+  }
+
+  return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-            <IonButton onClick={() => doLogin()} expand="full">
-                Login
-            </IonButton>
-        </IonContent>
+          <IonHeader>
+              <IonToolbar>
+                  <IonTitle>Login</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent className='ion-padding'>
+              <IonItem>
+                  <IonLabel position="floating">Username</IonLabel>
+                  <IonInput 
+                      value={username} 
+                      onIonChange={e => setUsername(e.detail.value!)} 
+                      required 
+                  />
+              </IonItem>
+              <IonItem>
+                  <IonLabel position="floating">Password</IonLabel>
+                  <IonInput 
+                      type="password" 
+                      value={password} 
+                      onIonChange={e => setPassword(e.detail.value!)} 
+                      required 
+                  />
+              </IonItem>
+              <IonButton onClick={doLogin} expand="full" className="ion-margin-top">
+                  Login
+              </IonButton>
+          </IonContent>
       </IonPage>
-    );
-  };
-  
-  export default Login;
+  );
+};
+
+export default Login;
