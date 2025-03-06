41e57f2c9b096a9070e87473c5ebb4333a4bf54a
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-03-06 20:06:14 +0800

    added badges on favorites.tsx

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -5,7 +5,8 @@ import {
       IonMenuButton, 
       IonPage, 
       IonTitle, 
-      IonToolbar 
+      IonToolbar,
+      IonBadge, IonItem, IonLabel, IonList  
   } from '@ionic/react';
   
   const Favorites: React.FC = () => {
@@ -19,18 +20,32 @@ import {
             <IonTitle>Favorites</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Favorites
-          </div>
-        </IonContent>
+        <IonList>
+      <IonItem>
+        <IonLabel>Followers</IonLabel>
+        <IonBadge color="primary">22k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Likes</IonLabel>
+        <IonBadge color="secondary">118k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Stars</IonLabel>
+        <IonBadge color="tertiary">34k</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Completed</IonLabel>
+        <IonBadge color="success">80</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Warnings</IonLabel>
+        <IonBadge color="warning">70</IonBadge>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Notifications</IonLabel>
+        <IonBadge color="danger">1000</IonBadge>
+      </IonItem>
+    </IonList>
       </IonPage>
     );
   };
