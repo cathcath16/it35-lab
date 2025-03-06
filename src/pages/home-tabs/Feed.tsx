f7a9d1eb427768caedacb354a9a83b01c1d5a94e
cathcath16 <catherenelabinisia@gmail.com>
2025-03-06 21:00:27 +0800

    added ion chip on feed.tsx

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -5,7 +5,8 @@ import {
       IonMenuButton, 
       IonPage, 
       IonTitle, 
-      IonToolbar 
+      IonToolbar,
+      IonChip 
   } from '@ionic/react';
   
   const Feed: React.FC = () => {
@@ -19,18 +20,16 @@ import {
             <IonTitle>Feed</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Feed
-          </div>
-        </IonContent>
+        <IonChip>Default</IonChip>
+      <IonChip color="primary">Primary</IonChip>
+      <IonChip color="secondary">Secondary</IonChip>
+      <IonChip color="tertiary">Tertiary</IonChip>
+      <IonChip color="success">Success</IonChip>
+      <IonChip color="warning">Warning</IonChip>
+      <IonChip color="danger">Danger</IonChip>
+      <IonChip color="light">Light</IonChip>
+      <IonChip color="medium">Medium</IonChip>
+      <IonChip color="dark">Dark</IonChip>
       </IonPage>
     );
   };
