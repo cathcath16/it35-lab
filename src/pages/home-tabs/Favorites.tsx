5a6d953ed415395c3a9ad4c0b60f401e31d2af9d
jaysashgundaya16 <jaysashgundaya17@gmail.com>
2025-03-06 20:23:08 +0800

    added image and created favorites.css

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -1,53 +1,64 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar,
-      IonBadge, IonItem, IonLabel, IonList  
-  } from '@ionic/react';
-  
-  const Favorites: React.FC = () => {
-    return (
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar,
+  IonBadge, IonItem, IonLabel, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle 
+} from '@ionic/react';
+import './Favorites.css'; // Import your CSS file for custom styles
+
+const Favorites: React.FC = () => {
+  return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Favorites</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonList>
-      <IonItem>
-        <IonLabel>Followers</IonLabel>
-        <IonBadge color="primary">22k</IonBadge>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Likes</IonLabel>
-        <IonBadge color="secondary">118k</IonBadge>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Stars</IonLabel>
-        <IonBadge color="tertiary">34k</IonBadge>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Completed</IonLabel>
-        <IonBadge color="success">80</IonBadge>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Warnings</IonLabel>
-        <IonBadge color="warning">70</IonBadge>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Notifications</IonLabel>
-        <IonBadge color="danger">1000</IonBadge>
-      </IonItem>
-    </IonList>
+          <IonHeader>
+              <IonToolbar>
+                  <IonButtons slot='start'>
+                      <IonMenuButton></IonMenuButton>
+                  </IonButtons>
+                  <IonTitle>Favorites</IonTitle>
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJBavOOQ_f_xSRFBYXPTdTAvxJ2E8k3FgWfg&s" alt="Instagram Logo" className="instagram-logo" /> {/* Adjust the path as necessary */}
+              </IonToolbar>
+          </IonHeader>
+          <IonCard>
+              <IonCardHeader>
+                  <IonCardTitle>INSTAGRAM</IonCardTitle>
+                  <IonCardSubtitle>Catherene Labinisia</IonCardSubtitle>
+              </IonCardHeader>
+
+              <IonCardContent>🎨 Making pixels look pretty
+              🌏 Exploring design & cultures</IonCardContent>
+          </IonCard>
+          <IonList>
+              <IonItem>
+                  <IonLabel>Followers</IonLabel>
+                  <IonBadge color="primary">22k</IonBadge>
+              </IonItem>
+              <IonItem>
+                  <IonLabel>Likes</IonLabel>
+                  <IonBadge color="secondary">118k</IonBadge>
+              </IonItem>
+              <IonItem>
+                  <IonLabel>Stars</IonLabel>
+                  <IonBadge color="tertiary">34k</IonBadge>
+              </IonItem>
+              <IonItem>
+                  <IonLabel>Completed</IonLabel>
+                  <IonBadge color="success">80</IonBadge>
+              </IonItem>
+              <IonItem>
+                  <IonLabel>Warnings</IonLabel>
+                  <IonBadge color="warning">70</IonBadge>
+              </IonItem>
+              <IonItem>
+                  <IonLabel>Notifications</IonLabel>
+                  <IonBadge color="danger">1000</IonBadge>
+              </IonItem>
+          </IonList>
       </IonPage>
-    );
-  };
-  
-  export default Favorites;
+  );
+};
+
+export default Favorites;
