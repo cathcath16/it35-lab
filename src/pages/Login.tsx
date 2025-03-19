d7c07d6d2dce183a8a93a124d391b3ce9dd08230
GundayaFarah <farahgundaya123@gmail.com>
2025-03-20 04:52:47 +0800

    added registration form and also register.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,64 +1,77 @@
+import React, { useState } from 'react';
 import { 
   IonButton,
   IonContent, 
   IonHeader, 
-  IonInput, 
-  IonItem, 
-  IonLabel, 
   IonPage, 
   IonTitle, 
   IonToolbar, 
-  useIonRouter 
+  IonInput,
+  IonItem,
+  IonLabel,
+  useIonRouter,
+  IonAlert
 } from '@ionic/react';
-import { useState } from 'react';
 import './Login.css'; 
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
-  
-  
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
 
   const doLogin = () => {
-      
-      console.log('Username:', username);
-      console.log('Password:', password);
-      
-      
+    if (username === '' || password === '') {
+      setShowAlert(true);
+    } else {
       navigation.push('/it35-lab/app', 'forward', 'replace');
+    }
+  }
+
+  const goToRegister = () => {
+    navigation.push('/register', 'forward', 'replace'); // Navigate to the Register page
   }
 
   return (
-      <IonPage>
-          <IonHeader>
-              <IonToolbar>
-                  <IonTitle>Login</IonTitle>
-              </IonToolbar>
-          </IonHeader>
-          <IonContent className='ion-padding page-background'> 
-              <IonItem>
-                  <IonLabel position="floating">Username</IonLabel>
-                  <IonInput 
-                      value={username} 
-                      onIonChange={e => setUsername(e.detail.value!)} 
-                      required 
-                  />
-              </IonItem>
-              <IonItem>
-                  <IonLabel position="floating">Password</IonLabel>
-                  <IonInput 
-                      type="password" 
-                      value={password} 
-                      onIonChange={e => setPassword(e.detail.value!)} 
-                      required 
-                  />
-              </IonItem>
-              <IonButton onClick={doLogin} expand="full" className="ion-margin-top">
-                  Login
-              </IonButton>
-          </IonContent>
-      </IonPage>    
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding background-image'>
+        <IonItem>
+          <IonLabel position="floating">Username</IonLabel>
+          <IonInput 
+            value={username} 
+            onIonChange={e => setUsername(e.detail.value!)} 
+            required 
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Password</IonLabel>
+          <IonInput 
+            type="password" 
+            value={password} 
+            onIonChange={e => setPassword(e.detail.value!)} 
+            required 
+          />
+        </IonItem>
+        <IonButton onClick={doLogin} expand="full" className="ion-margin-top">
+          Login
+        </IonButton>
+        <IonButton onClick={goToRegister} expand="full" className="ion-margin-top" fill="clear">
+          DON'T HAVE AN ACCOUNT? REGISTER HERE
+        </IonButton>
+        <IonAlert
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          header={'Error'}
+          message={'Please enter both username and password.'}
+          buttons={['OK']}
+        />
+      </IonContent>
+    </IonPage>
   );
 };
 
