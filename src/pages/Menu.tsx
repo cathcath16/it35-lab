263c20e3b83bd3cc992bc869e51e52161b550537
GundayaFarah <farahgundaya123@gmail.com>
2025-04-28 03:15:14 +0800

    added the updated in menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -14,18 +14,21 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react'
-  import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
-import { Redirect, Route } from 'react-router';
-import Home from './Home';
-import About from './About';
-import Details from './Details';
-
+  import {homeOutline, logOutOutline, rocketOutline, settingsOutline} from 'ionicons/icons';
+  import { Redirect, Route } from 'react-router';
+  import Home from './Home';
+  import About from './About';
+  import Details from './Details';
+  import { supabase } from '../utils/supabaseClient';
+  import EditProfilePage from './EditProfilePage';
+  
   const Menu: React.FC = () => {
     const path = [
         {name:'Home', url: '/it35-lab/app/home', icon: homeOutline},
         {name:'About', url: '/it35-lab/app/about', icon: rocketOutline},
+        {name:'Profile', url: '/it35-lab/app/profile', icon: settingsOutline},
     ]
-
+  
     return (
         <IonPage>
             <IonSplitPane contentId="main">
@@ -46,7 +49,7 @@ import Details from './Details';
                                 </IonItem>
                             </IonMenuToggle>
                         ))}
-
+  
                         {/*Logout Button*/}
                         <IonButton routerLink="/it35-lab" routerDirection="back" expand="full">
                             <IonIcon icon={logOutOutline} slot="start"> </IonIcon>
@@ -60,12 +63,14 @@ import Details from './Details';
                     <Route exact path="/it35-lab/app/home" component={Home} />
                     <Route exact path="/it35-lab/app/home/details" component={Details} />
                     <Route exact path="/it35-lab/app/about" component={About} />
-
+                    <Route exact path="/it35-lab/app/profile" component={EditProfilePage} />
+  
+  
                     <Route exact path="/it35-lab/app">
                         <Redirect to="/it35-lab/app/home"/>
                     </Route>
                 </IonRouterOutlet>
-
+  
             </IonSplitPane>
         </IonPage>
     );
