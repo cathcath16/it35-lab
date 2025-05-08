23ade5d44d023559cbf1f03e9480a4b9dbf86322
GundayaFarah <farahgundaya123@gmail.com>
2025-05-08 15:06:48 +0800

    Updates

--- assets/ios.transition-legacy-CbOEh8IY.js
+++ assets/ios.transition-legacy-CbOEh8IY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BEPrsd72.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Ddr8dlgj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
